eda195681e27dbb500c786272abab11d1c6c0f7c
arne.gockeln <arne@gockeln.com>
2018-06-11 16:41:34 +0200

    improvements

--- reqdeb.php
+++ reqdeb.php
@@ -1,45 +1,61 @@
+#!/usr/bin/php
 <?php
 /**
  * This script sends n requests to a url and searches its response for phrases.
  * Author: Arne Gockeln <arne@gockeln.com>
  */ 
 if($argc == 1){
-	printf("Usage: %s [-nuos]\n\t[-n num] Send num requests\n\t[-u http...] The url to request\n\t[-o] Output every response instead on hit only\n\t[-s phrase1,phrase2,...] A comma seperated list of phrases\n", $argv[0]);
+	$params = array(
+		'[-n num]' => 'Send num requests, default is 5',
+		'[-s phrase1,phrase2,...]' => 'A comma seperated list of phrases',
+		'[-h header1,header2,...]' => 'A comma seperated list of headers',
+		'[-o]' => 'Output every response instead on hit only',
+		'[-j]' => 'Json response is expected'
+	);
+	printf("Usage: %s [-n:s:h:jo] http...\n", $argv[0]);
+	foreach($params as $param => $param_desc){
+		printf("\t%s %s\n", $param, $param_desc);
+	}
 	exit();
 }
 
-$request_count = 10;
-$request_host = 'https://qt.buzz09.de';
-//$request_url = $request_host . '/wp-json/appbackend/v1/posts/0';
-$request_url = $request_host . '/wp-json/appbackend/v1/config';
+$request_count = 5;
+$request_url = $argv[ $argc - 1 ];
 $search_phrases = ['Datenbankverbindung', 'Fehler'];
 
 // ----------------------------------
-
+// default headers
 $request_headers = [];
 $request_headers[] = 'AppBackendToken: my_awesome_token';
-$request_headers[] = 'Content-Type: application/json; charset=utf-8';
 $request_headers[] = 'Language: de';
 
-// -n 10 => send 10 requests
-// -o => output response
-// -u http... => the url to fetch
-// -s search1,search2... => search these phrases
-$options = getopt('n:ou:s:');
+// parse options
+$options = getopt('n:ou:s:h:j');
+// set request count, defaults to 10
 if(isset($options['n'])){
 	if((int)$options['n'] > 0){
 		$request_count = (int) $options['n'];
 	}
 }
+
+// set output option, defaults to false
 $doOutput = false;
 if(isset($options['o'])){
 	$doOutput = true;
 }
+
+// sets url
 if(isset($options['u'])){
 	if(strlen($options['u']) > 0 && stripos($options['u'], 'http') !== false){
 		$request_url = $options['u'];
 	}
 }
+if( empty($request_url) ){
+	printf("Error: no url found.\n");
+	exit();
+}
+
+// set search phrases
 if(isset($options['s']) && strlen($options['s']) > 0){
 	$search_phrases = explode(',', $options['s']);
 	if(!is_array($search_phrases)){
@@ -47,14 +63,38 @@ if(isset($options['s']) && strlen($options['s']) > 0){
 	}
 }
 
+// set headers
+if(isset($options['h']) && strlen($options['h']) > 0){
+	$headers = explode(',', $options['h']);
+	if(!is_array($headers)){
+		$headers = array($options['h']);
+	}
+
+	foreach($headers as $plus_header){
+		$request_headers[] = $plus_header;
+	}
+}
+
+// set json request header
+if(isset($options['j'])){
+	$request_headers[] = 'Content-Type: application/json; charset=utf-8';
+} else {
+	$request_headers[] = 'Content-Type: text/html; charset=utf-8';
+}
+
 $search_phrases_c = count($search_phrases);
+$request_headers_c = count($request_headers);
 
 $total_time = 0;
 $total_hits = 0;
-printf("Send %s requests to: %s\n", $request_count, $request_url);
-printf("Search each response for: \n");
-for($i = 0; $i < $search_phrases_c; $i++){
-	printf("- %s\n", $search_phrases[$i]);
+printf("[Config]\n- %s Requests\n- Url: %s\n", $request_count, $request_url);
+printf("[Headers]\n");
+for($i0 = 0; $i0 < $request_headers_c; $i0++){
+	printf("- %s\n", $request_headers[$i0]);
+}
+printf("[Search each response] \n");
+for($i1 = 0; $i1 < $search_phrases_c; $i1++){
+	printf("- %s\n", $search_phrases[$i1]);
 }
 printf("--------------------------------------------\n");
 for($i = 0; $i < $request_count; $i++){
