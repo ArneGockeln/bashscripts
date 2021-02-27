e9deb1df712ee793808cdcb69cf4e684004089df
Arne Gockeln <arne@gockeln.com>
2021-02-27 19:15:04 +0100

    add: update command

--- site.sh
+++ site.sh
@@ -5,10 +5,11 @@
 # site build    Use it to build for production
 # site run      Use it to serve a jekyll site
 # site install  Use it to install jekyll gems
+# site update   Use it to update jekyll gems
 # site clean    Use it to clean the site and metadata files
 # 
 # Author: Arne Gockeln
-# Version: 0.3
+# Version: 0.4
 # 
 
 CMD=$1
@@ -16,6 +17,7 @@ BUILDCMD="JEKYLL_ENV=production bundle exec jekyll build --trace"
 RUNCMD="JEKYLL_ENV=development bundle exec jekyll serve --trace --draft"
 INSTALLCMD="bundle install"
 CLEANCMD="bundle exec jekyll clean"
+UPDATECMD="bundle update"
 
 # check if we are inside a jekyll directory
 JEKYLL=$(find . -maxdepth 1 -name _config.yml)
@@ -42,12 +44,16 @@ case "$CMD" in
     eval $INSTALLCMD
     ;;
 
+    update)
+    eval $UPDATECMD
+    ;;
+
     clean)
     eval $CLEANCMD
     ;;
 
     *)
-    echo $"Usage: $0 {run|build|install|clean}"
+    echo $"Usage: $0 {run|build|install|update|clean}"
     exit 1
 esac
 
